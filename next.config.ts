535a4c4bb45c76b78561815369380dd81c55c81f
Ingo Wolf <me@ingowolf.au>
2025-09-24 22:03:48 +1000

    remove rewrites because they are not working

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: "/edutools/:path*",
-        destination: "https://edutools.ingo.au/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
